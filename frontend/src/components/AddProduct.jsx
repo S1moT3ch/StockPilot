bcb3ac603e93f3167d5cb9d7a75c00e68ccd10b9
S1moT3ch <s.cantore2@studenti.poliba.it>
2025-09-01 11:48:36 +0200

    Update AddProduct.jsx

--- frontend/src/components/AddProduct.jsx
+++ frontend/src/components/AddProduct.jsx
@@ -83,7 +83,7 @@ const AddProduct = ({ categories, setCategories, handleCloseAdd, setProduct, set
                 descrizione: prodotto.descrizione,
                 quantità: prodotto.quantità,
                 ubicazione: prodotto.ubicazione,
-                categoria: prodotto.categoria ? prodotto.categoria.nome : '',
+                categoria: prodotto.categoria.nome ? prodotto.categoria.nome : '',
                 segnalazione: prodotto.segnalazione,
             }]);
         } catch (error) {
